ec2896c43c2d92863e2956ca05dfcbeedb3601e5
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-07-15 16:20:19 +0300

    Updates

---
+++ precache-manifest.a069bcb5f74481567b0b116fc453e997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625193a2f210c90638b451f658b9a795",
+    "url": "/React-game/index.html"
+  },
+  {
+    "revision": "dc66403db294b6545291",
+    "url": "/React-game/static/css/main.eccd430b.chunk.css"
+  },
+  {
+    "revision": "3f19e69d1c6a492396e7",
+    "url": "/React-game/static/js/2.a61b71c8.chunk.js"
+  },
+  {
+    "revision": "dc66403db294b6545291",
+    "url": "/React-game/static/js/main.d964d454.chunk.js"
+  },
+  {
+    "revision": "cb2ffbd55fa9d9d408a5",
+    "url": "/React-game/static/js/runtime~main.e3737006.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
